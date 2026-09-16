19aadba1283f64562b802b13952d29af17993f4b
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-09-15 21:31:47 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/PMI-P1-Grupo1/", // trocar pelo nome exato do repositório
 })
